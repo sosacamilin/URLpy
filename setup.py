89fc9692007716bb16c87837d57bb384ed88e65c
Camilo Sosa Morales <codex.deb@gmail.com>
2012-08-06 02:40:31 -0500

    reconstruction of code.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = "URLpy", 
+	  version = "0.1", 
+	  description = "A python wrapper for download files, from url or text file.", 
+	  author = "Camilo Sosa Morales.", 
+	  author_email = "codex.deb@gmail.com", 
+	  url = "http://sosacamilin.github.com/URLpy/", 
+	  license = "GPL", 
+	  scripts = ["urlpy.py"], 
+)
--- urlpy.py
+++ urlpy.py
@@ -1,78 +1,103 @@
+#!/usr/bin/env python
+
 import urllib
+import urllib2
 import sys
 import os
 import getopt
+import time
+
+class URLpy():
+    """
+    Class to download files individually or in a text file.
+    """
+    def __init__(self):
+        self.aux = 0
+        self.dir = os.getcwd()
+        self.simbol = '%'
 
 
-class Url():
-	"""Class to download files individually or in a text file."""
-	def __init__(self):
-		self.aux = 0
+    def usage(self):
+        print '''
+Camilo Sosa Morales. a.k.a c0dex__ codex.deb@gmail.com
+Facebook: fb.com/kamiadry
+---------------------
+Usage: python url.py [options...] <URL> or <fileName>
+
+Options:    
+-u     URL file to download.
+-f     file name with links to download.
+        '''
 
 
-	def usage(self):
-		print("""
-Camilo Sosa Morales <codex.deb@gmail.com>
-Usage: python url.py [options...] <url> or <file>
-
-Options:
--u     url file to download.
--f     file with links to download.
-		""")
-
-
-	def download(self, urlFile):
-		"""download the file with the url or links sent."""
-		try:
-			print("- Downloading... %s" % os.path.basename(urlFile))
-			urllib.urlretrieve(urlFile, os.path.basename(urlFile), self.progress)
-			urllib.urlcleanup() #clears the cache urllib.urlretrieve()
-			self.aux = 0
-			print("\n- Download complete.\n")
-		except IOError, e:
-			print(e)
+    def download(self, urlFile):
+        """
+        Download the file with the url or links sent.
+        """
+        try:
+            r = urllib2.urlopen(urlFile)
+            print '\n[?] File information: %s - %.2f MB' % (os.path.basename(urlFile),
+                float(r.info()['Content-Length']) / 1048576)
+            urllib.urlretrieve(urlFile, os.path.basename(urlFile), self.progress)
+            urllib.urlcleanup() #clears the cache urllib.urlretrieve()
+            self.aux = 0
+            sys.stdout.write(' - Done.\n')
+        except IOError, e:
+            print e
+        except KeyboardInterrupt:
+            print '\n[x] Download canceled by user.'
 
 
-	def progress(self, numBlock, blockSize, totalSize):
-		"""progress() function, gives the percentage of the file to download and send it to the function percent()."""
-		p = int(numBlock * blockSize * 100 / totalSize)
-		self.percent(p)
+    def progress(self, numBlock, blockSize, totalSize):
+        """
+        Progress() function, gives the percentage of the file to download and send it to the function percent().
+        """
+        p = int(numBlock * blockSize * 100 / totalSize)
+        self.percent(p)
 
 
-	def percent(self, percent):
-		"""screen printed on the progress of the download."""
-		if percent == self.aux:
-			sys.stdout.write("|")
-			sys.stdout.flush()
-			self.aux += 1
+    def percent(self, percent):
+        """
+        Screen printed on the progress of the download.
+        """
+        if percent == self.aux:
+            sys.stdout.write('\r\t[~] Downloading ... %d%s' % (percent, self.simbol))
+            sys.stdout.flush()
+            time.sleep(.125)
+            self.aux += 1
 
 
-	def readFile(self, nameFile):
-		"""read a file with links for dowload"""
-		try:
-			f = open(nameFile, "r")
-			flist = f.readlines() #returns a list of links
-			f.close()
-			for l in flist:
-				self.download(l)
-		except IOError, e:
-			print(e)
+    def readFile(self, nameFile):
+        """
+        Read a file with links for dowload.
+        """
+        try:
+            f = open(nameFile, 'r')
+            flist = f.readlines() #returns a list of links
+            f.close()
+            for l in flist:
+                self.download(a, l)
+        except IOError, e:
+            print(e)
 
 
-	def main(self):
-		try:
-			opts, args = getopt.getopt(sys.argv[1:], "hd:f:")
-			for o, a in opts:
-				if o in ("-h"):
-					self.usage()
-				if o in ("-d"):
-					self.download(sys.argv[2])
-				if o in ("-f"):
-					self.readFile(sys.argv[2])
-		except getopt.error, e:
-			print(e)
+    def main(self):
+        """
+        Menu of options.
+        """
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], 'hd:f:')
+            for o, a in opts:
+                if o in ('-h'):
+                    self.usage()
+                if o in ('-d'):
+                    self.download(sys.argv[2])
+                if o in ('-f'):
+                    self.readFile(sys.argv[2:])
+        except getopt.error, e:
+            print(e)
 
 
 if __name__ == '__main__':
-	u = Url()
-	u.main()		
+    urlpy = URLpy()
+    urlpy.main()        
