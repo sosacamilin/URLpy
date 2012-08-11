9fe3cdd001ae08271394d00ee7e9ad289f532b20
Camilo Sosa Morales <codex.deb@gmail.com>
2012-08-10 19:44:30 -0500

    removed raise, changes in the functions download(), progress() and percent().

--- urlpy.py
+++ urlpy.py
@@ -14,26 +14,36 @@ class URLpy():
     def __init__(self):
         self.aux = 0
         self.dir = os.getcwd()
-        self.simbol = '%'
+        self.simbol = "%"
 
     def download(self, urlFile):
         """
         Download the file with the url or links sent.
         """
-        print type(urlFile)
-        print urlFile
         try:
-            print '\n[?] File information: %s saved in %s' % (os.path.basename(urlFile), self.dir)
+            print("\n[?] File information: %s saved in %s") % (os.path.basename(urlFile), self.dir)
             urllib.urlretrieve(urlFile, os.path.basename(urlFile), self.progress)
             urllib.urlcleanup() #clears the cache urllib.urlretrieve()
             self.aux = 0
-            sys.stdout.write(' - Done.\n')
+            sys.stdout.write(" - Done.\n")
             self.beep()
         except IOError, er:
-            print er
+            print(er)
         except KeyboardInterrupt:
-            print '\n[x] Download canceled by user.'
-        raise urllib.ContentTooShortError('testeando....')
+            print("\n[x] Download canceled by user.")
+
+    def readFile(self, nameFile):
+        """
+        Read a file with links for dowload.
+        """
+        try:
+            file_ = open(nameFile, "r")
+            link = file_.readlines() #returns a list of links
+            file_.close()
+            for l in link:
+                self.download(l)
+        except IOError, e:
+            print(e)
 
     def progress(self, numBlock, blockSize, totalSize):
         """
@@ -48,41 +58,28 @@ class URLpy():
         Screen printed on the progress of the download.
         """
         if percent == self.aux:
-            sys.stdout.write('\r\t[~] [%s/%s] Downloading ... %d%s' % (self.sizeFile(percent_), self.sizeFile(size),
-                                                                       percent, self.simbol))
+            sys.stdout.write("\r\t[~] [%s / %s] Downloading ... %d%s" % (self.sizeFile(percent_), self.sizeFile(size),
+                                                                         percent, self.simbol))
             sys.stdout.flush()
             time.sleep(.125)
             self.aux += 1
-
-    def readFile(self, nameFile):
-        """
-        Read a file with links for dowload.
-        """
-        try:
-            file_ = open(nameFile, 'r')
-            link = f.readlines() #returns a list of links
-            file_.close()
-            for l in link:
-                self.download(l)
-        except IOError, e:
-            print e
 
     def sizeFile(self, bytes):
         bytes = float(bytes)
         if bytes >= 1099511627776:
             teraBytes = bytes / 1099511627776
-            size = '%.1f TB' % teraBytes
+            size = "%.1fTB" % teraBytes
         elif bytes >= 1073741824:
             gigaBytes = bytes / 1073741824
-            size = '%.1f GB' % gigaBytes
+            size = "%.1fGB" % gigaBytes
         elif bytes >= 1048576:
             megaBytes = bytes / 1048576
-            size = '%.1f MB' % megaBytes
+            size = "%.1fMB" % megaBytes
         elif bytes >= 1024:
             kiloBytes = bytes / 1024
-            size = '%.1f KB' % kiloBytes
+            size = "%.0fKB" % kiloBytes
         else:
-            size = '%.1f Bytes' % bytes
+            size = "%.1f Bytes" % bytes
         return size
 
     def beep(self):
@@ -97,29 +94,27 @@ class URLpy():
         Menu of options.
         """
         try:
-            opts, args = getopt.getopt(sys.argv[1:], 'hd:f:')
+            opts, args = getopt.getopt(sys.argv[1:], "hdf")
             for o, a in opts:
-                if o in ('-h'):
+                if o in ("-h"):
                     self.usage()
-                if o in ('-d'):
+                if o in ("-d"):
                     self.download(sys.argv[2])
-                if o in ('-f'):
+                if o in ("-f"):
                     self.readFile(sys.argv[2])
         except getopt.error, er:
-            print er
+            print(er)
 
     def usage(self):
-        print '''
-Camilo Sosa Morales. a.k.a c0dex__ codex.deb@gmail.com
-Twitter: @sosacamilin
----------------------
-Usage: python url.py [options...] <URL> or <fileName>
+        print("""
+        URLpy - Developer by @sosacamilin
 
-Options:    
--u     URL file to download.
--f     file name with links to download.
-        '''
+        Usage: urlpy [options...] <URL> or <fileName>
+        Options:    
+        -d     URL file to download.
+        -f     file name with links to download.
+        """)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     urlpy = URLpy()
     urlpy.main()
